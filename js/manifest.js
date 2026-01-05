57cdac92570713e80268e0f01de1220a5a3206e1
火龙果 <3342959285@qq.com>
2026-01-05 23:42:52 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '69511bace297b286b8057916';
+_mrmcp['creative_id'] = '695298eafb1d57c32d081447';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 960;
+_mrmcp['height'] = _mrmcp['height'] || 540;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '1';
+_mrmcp['build_number'] = parseInt('585');
+_mrmcp['publish_time'] = 'Mon Jan 05 2026 22:21:05 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.com";
+})();
